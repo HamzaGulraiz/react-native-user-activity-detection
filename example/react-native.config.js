11a3ef1cdd2813861fcc7a2ffc8be4f25cc8af6c
HamzaGulraiz <devhamzagulraiz@gmail.com>
2025-06-12 08:05:16 -0700

    feat: example project added

---
+++ example/react-native.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  dependencies: {
+    'react-native-user-activity-lib': {
+      platforms: {
+        ios: {
+          podspecPath: 'react-native-user-activity-detection.podspec',
+        },
+      },
+    },
+  },
+};
